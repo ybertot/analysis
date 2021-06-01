823f8e5667f3f6e4b007c44d77622177709a1ac2
thery <Laurent.Thery@inria.fr>
2021-06-01 03:34:28 +0200

    nsatz on a separate file

--- theories/exp.v
+++ theories/exp.v
@@ -1,8 +1,8 @@
 (* mathcomp analysis (c) 2017 Inria and AIST. License: CeCILL-C.              *)
-Require Nsatz NsatzTactic.
 From mathcomp Require Import all_ssreflect ssralg ssrint ssrnum.
 From mathcomp Require Import matrix interval rat.
 Require Import boolp reals ereal.
+Require Import nsatz_realtype.
 Require Import classical_sets posnum topology normedtype landau sequences.
 Require Import derive.
 
@@ -306,72 +306,6 @@ repeat  rewrite /(@GRing.mul (fct_ringType _ _)) /=
           /(@GRing.opp (fct_ringType _ _)) /=
           /(@GRing.opp (fct_zmodType _ _)) /=.
 
-(******************************************************************************)
-(* Just to have ring for realType                                             *)
-(******************************************************************************)
-
-Section Nsatz_realType.
-Variable T : realType.
-
-Lemma Nsatz_realType_Setoid_Theory : Setoid.Setoid_Theory T (@eq T).
-Proof. by constructor => [x //|x y //|x y z ->]. Qed.
-
-Definition Nsatz_realType0 := (0%:R : T).
-Definition Nsatz_realType1 := (1%:R : T).
-Definition Nsatz_realType_add (x y : T) := (x + y)%R.
-Definition Nsatz_realType_mul (x y : T) := (x * y)%R.
-Definition Nsatz_realType_sub (x y : T) := (x - y)%R.
-Definition Nsatz_realType_opp (x  : T) := (- x)%R.
-
-#[global]
-Instance Nsatz_realType_Ring_ops:
-   (@Ncring.Ring_ops T Nsatz_realType0 Nsatz_realType1
-  Nsatz_realType_add
-  Nsatz_realType_mul
-  Nsatz_realType_sub
-  Nsatz_realType_opp (@eq T)).
-Defined.
-
-#[global]
-Instance Nsatz_realType_Ring : (Ncring.Ring (Ro:=Nsatz_realType_Ring_ops)).
-Proof.
-constructor => //.
-- exact: Nsatz_realType_Setoid_Theory.
-- by move=> x y -> x1 y1 ->.
-- by move=> x y -> x1 y1 ->.
-- by move=> x y -> x1 y1 ->.
-- by move=> x y ->.
-- exact: add0r.
-- exact: addrC.
-- exact: addrA.
-- exact: mul1r.
-- exact: mulr1.
-- exact: mulrA.
-- exact: mulrDl.
-- move=> x y z; exact: mulrDr.
-- exact: subrr.
-Defined.
-
-#[global]
-Instance Nsatz_realType_Cring: (Cring.Cring (Rr:=Nsatz_realType_Ring)).
-Proof. exact: mulrC. Defined.
-
-Locate Integral_domain.
-#[global]
-Instance Nsatz_realType_Integral_domain :
-   (Integral_domain.Integral_domain (Rcr:=Nsatz_realType_Cring)).
-Proof.
-constructor.
-  move=> x y.
-  rewrite -[_ _ Algebra_syntax.zero]/(x * y = 0)%R => /eqP.
-  by rewrite mulf_eq0 => /orP[] /eqP->; [left | right].
-rewrite -[_ _ Algebra_syntax.zero]/(1 = 0)%R; apply/eqP.
-by rewrite (eqr_nat T 1 0).
-Defined.
-
-End Nsatz_realType.
-
-Tactic Notation "nsatz" := NsatzTactic.nsatz_default.
 
 (******************************************************************************)
 (* Differentiability of series inspired by HOL-Light transc.ml                *)
@@ -1265,5 +1199,3 @@ Definition pi :=
   then (projT1 (cid e)) *+ 2 else 0.
 
 End Pi.
-  
-End CosSin.
